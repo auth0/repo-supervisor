e2608dbff9b95074462265d39355a0172856ae0f
radekk <radekk@auth0.com>
2017-02-20 18:27:23 +0100

    Extract webhook trigger to separate file.

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 'use strict';
 
+/* eslint consistent-return: off */
+
 const assert = require('assert');
 const config = require('../config/main.json');
-const wh = require('./parser/webhook');
+const webhook = require('./webhook');
 
 module.exports = (ctx, req, res) => {
   assert(ctx.data, 'Invalid request - missing query parameters.');
@@ -19,39 +21,7 @@ module.exports = (ctx, req, res) => {
     return viewer.getReportContent(ctx.data.id).then(respond).catch(respond);
   }
 
-  const repository = {
-    name: ctx.data.repository.name,
-    owner: ctx.data.repository.owner.login,
-    pullRequestId: ctx.data.pull_request.number,
-    pullRequestSHA: ctx.data.pull_request.head.sha
-  };
-
   if (config.pullRequests.allowedActions.indexOf(ctx.data.action) > -1) {
-    // Webhook trigger.
-    const status = require('./helpers/status')(github, {
-      repo: repository.name,
-      user: repository.owner,
-      sha: repository.pullRequestSHA
-    });
-
-    return wh.parse(
-      github,
-      repository.pullRequestId,
-      repository.owner,
-      repository.name,
-      true
-    ).then(data =>
-      status.setPending('Checking for security issues...').then(() => {
-        if (data.issues.length > 0) {
-          return status.setError(
-            'Security issues were detected.',
-            viewer.getReportURL(repository.pullRequestId, repository.owner, repository.name)
-          );
-        }
-
-        return status.setSuccess('Everything looks fine, no issues detected.');
-      }).then(respond)
-        .catch(err => status.setFailure(err.toString()).then(respond))
-    ).catch(err => respond(err.toString()));
+    return webhook(ctx, github, viewer, respond);
   }
 };
---
+++ src/webhook.js
@@ -0,0 +1,37 @@
+const webhook = require('./parser/webhook');
+
+module.exports = (ctx, github, viewer, res) => {
+  const repository = {
+    name: ctx.data.repository.name,
+    owner: ctx.data.repository.owner.login,
+    pullRequestId: ctx.data.pull_request.number,
+    pullRequestSHA: ctx.data.pull_request.head.sha
+  };
+
+  // Webhook trigger.
+  const status = require('./helpers/status')(github, {
+    repo: repository.name,
+    user: repository.owner,
+    sha: repository.pullRequestSHA
+  });
+
+  return webhook.parse(
+    github,
+    repository.pullRequestId,
+    repository.owner,
+    repository.name,
+    true
+  ).then(data =>
+    status.setPending('Checking for security issues...').then(() => {
+      if (data.issues.length > 0) {
+        return status.setError(
+          'Security issues were detected.',
+          viewer.getReportURL(repository.pullRequestId, repository.owner, repository.name)
+        );
+      }
+
+      return status.setSuccess('Everything looks fine, no issues detected.');
+    }).then(res)
+      .catch(err => status.setFailure(err.toString()).then(res))
+  ).catch(err => res(err.toString()));
+};
