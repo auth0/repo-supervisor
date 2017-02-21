9e88604dbaf99b18674b41d39a46244487d40c45
radekk <radekk@auth0.com>
2017-02-21 21:03:01 +0100

    Display always the same number of fake characters for secrets.

--- src/render/helpers/obfuscate.js
+++ src/render/helpers/obfuscate.js
@@ -1,10 +1,11 @@
 'use strict';
 
 module.exports = (config) => {
+  const numOfFakeChars = 20;
   const numOfChars = config.obfuscate.numOfVisibleCharacters;
 
   return {
     name: 'obfuscate',
-    cb: str => str.substring(0, numOfChars) + '*'.repeat(str.length - numOfChars)
+    cb: str => str.substring(0, numOfChars) + '*'.repeat(numOfFakeChars)
   };
 };
