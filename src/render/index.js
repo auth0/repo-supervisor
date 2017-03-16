fa94663e85f4546dd2d71f4e5b911084c6e74179
radekk <radekk@auth0.com>
2017-03-16 17:27:59 +0100

    Remove a source code comment that we left previously.

--- src/render/index.js
+++ src/render/index.js
@@ -4,14 +4,6 @@ import dataHelper from './helpers/data';
 const tpl = require('./templates/default.handlebars');
 
 module.exports = (data, rawOutput) => {
-  // Register helpers
-  // const helpers = require('./../../config/render/helpers.json');
-
-  // helpers.enabled.forEach((cfg) => {
-  //   const helper = require(`./templates/helpers/${cfg}`)(helpers.options);
-  //   hb.registerHelper(helper.name, helper.cb);
-  // });
-
   if (rawOutput) return data;
 
   // Group by filter
