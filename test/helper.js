29d93a5c2cda5c1663e6e1a894ffaa995014ebb6
Jason Tarka <jason@tarka.ca>
2018-01-18 16:17:00 -0500

    A variety of small fixes to non-production files
    - Don't flag as many ESLint errors in tests
    - Don't commit `dist/`
    - Make CLI tests set environment variables in a way that works on Windows
    - Make fixture loading work on Windows
      - Path variables use \, but module resolution can use /

--- test/helper.js
+++ test/helper.js
@@ -7,8 +7,15 @@ global.configs = {
   preFilters: require('./../config/filters/entropy.meter.json').options.preFilters
 };
 
-global.getFixtures = (filepath) =>
-  require(filepath.replace(/\/test\/(unit|integration)\//, '/test/fixtures/$1/').replace(/\.js$/, '.json'));
+global.getFixtures = (filepath) => {
+  if (filepath.includes('\\test\\')) {
+    // Windows uses \, but the regex needs /
+    filepath = filepath.replace(/\\/g, '/');
+  }
+
+  const jsonPath = filepath.replace(/\/test\/(unit|integration)\//, '/test/fixtures/$1/').replace(/\.js$/, '.json');
+  return require(jsonPath);
+};
 
 require('babel-core/register')({
   ignore: /node_modules\/(?!ProjectB)/
--- test/integration/src/cli.js
+++ test/integration/src/cli.js
@@ -15,11 +15,22 @@ describe('Scenario: Run tool in CLI mode to detect secrets', () => {
   it('should return an error in JSON format when triggered in JSON mode', (cb) => {
     const dir = './test/fixtures/integration/dir.without.any.files.to.test';
     const msg = 'Not detected any secrets in files.';
+    const options = {
+      env: {
+        JSON_OUTPUT: '1',
+        PATH: process.env.PATH
+      }
+    };
 
-    exec(`JSON_OUTPUT=1 node ./dist/cli.js ${dir}`, (error, stdout) => {
-      const jsonResult = JSON.parse(stdout);
+    exec(`node ./dist/cli.js ${dir}`, options, (error, stdout) => {
+      expect(error.code, 'exit code').to.be.equal(1);
 
-      expect(error.code).to.be.equal(1);
+      let jsonResult;
+      try {
+        jsonResult = JSON.parse(stdout);
+      } catch (e) {
+        throw new Error(`Invalid JSON: "${stdout}"`);
+      }
       expect(jsonResult.error.trim()).to.be.equal(msg);
       expect(Object.keys(jsonResult).length).to.be.equal(1);
       cb();
@@ -62,8 +73,14 @@ describe('Scenario: Run tool in CLI mode to detect secrets', () => {
   it('should detect secrets in supported files when in JSON mode', (cb) => {
     const dir = './test/fixtures/integration/dir.with.secrets';
     const msg = '{"result":{"./test/fixtures/integration/dir.with.secrets/foo/bar.js":["zJd-55qmsY6LD53CRTqnCr_g-","gm5yb-hJWRoS7ZJTi_YUj_tbU","GxC56B6x67anequGYNPsW_-TL","MLTk-BuGS8s6Tx9iK5zaL8a_W","2g877BA_TsE-WoPoWrjHah9ta"],"./test/fixtures/integration/dir.with.secrets/foo/foo.json":["d7kyociU24P9hJ_sYVkqzo-kE","q28Wt3nAmLt_3NGpqi2qz-jQ7"]}}';
+    const options = {
+      env: {
+        JSON_OUTPUT: '1',
+        PATH: process.env.PATH
+      }
+    };
 
-    exec(`JSON_OUTPUT=1 node ./dist/cli.js ${dir}`, (error, stdout) => {
+    exec(`node ./dist/cli.js ${dir}`, options, (error, stdout) => {
       expect(error).to.be.null;
       expect(stdout.trim()).to.be.equal(msg);
       cb();
