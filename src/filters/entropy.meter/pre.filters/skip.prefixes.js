bd0e2d3fd54da3bab096aa10dd1cba505a097614
Jason Tarka <jason@tarka.ca>
2018-01-18 16:18:12 -0500

    Use newer string & array method in filters to make the code more readable

--- src/filters/entropy.meter/pre.filters/skip.prefixes.js
+++ src/filters/entropy.meter/pre.filters/skip.prefixes.js
@@ -3,5 +3,4 @@
 /**
  * Skip strings with predefined prefixes.
  */
-module.exports = (s, config) => !!config.skipPrefixes.reduce((acc, prefix) =>
-  (acc &= !(s.indexOf(prefix) === 0)), 1);
+module.exports = (s, config) => !config.skipPrefixes.some(prefix => s.startsWith(prefix));
