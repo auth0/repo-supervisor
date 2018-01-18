1b130bcfac54b15be2f0ded87585e58d7c0004d9
Jason Tarka <jason@tarka.ca>
2018-01-18 15:05:40 -0500

    Reduce false positives: pre-filter camelCase and similar names
    This splits each string into individual words as best it can and compares it to a list of English words.
    If the string is 35% or more dictionary words it will filter it out.

---
+++ src/filters/entropy.meter/pre.filters/dictionary.words.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const dictionary = new Set(require('an-array-of-english-words'));
+
+const requiredPercent = 0.35;
+
+function splitIntoWords(str) {
+  // Hyphens, underscores, and numbers
+  str = str.replace(/[-_]/g, ' ')
+    .replace(/([0-9]+)/g, ' $1 ');
+
+  // camelCase
+  str = str.replace(/([A-Z]+?)([A-Z][a-z])/g, '$1 $2')
+    .replace(/([a-z])([A-Z])/g, '$1 $2')
+    .toLowerCase();
+
+  // Split into parts
+  return str.trim().split(/ +/);
+}
+
+/**
+ * Determine if a string is composed of dictionary words in camelCase or similar formats
+ * eg: MY_VARIABLE, myVariable, MyHTMLGenerator, an-identifier-string
+ *
+ * This reduces the number of variables, classes, and such that appear
+ *
+ * @param str {string} The input string
+ * @returns {boolean} True if it is not composed of words, false if it is
+ */
+module.exports = function filterCamelCaseWord(str) {
+  const words = splitIntoWords(str);
+
+  // Compare against the word list
+  const wordCount = words.reduce(
+    (acc, word) => acc + (word.length > 2 && dictionary.has(word)),
+    0
+  );
+
+  const percent = wordCount / words.length;
+  return percent < requiredPercent;
+};
