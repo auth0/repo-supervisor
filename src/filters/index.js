d98ea180863a2dd7b21c025e866655339d58f01e
radekk <radekk@auth0.com>
2020-03-04 14:20:08 +0100

    Feat: multiple file extensions for a single file type
    It adds a support for multiple file extensions for a single file type, i.e. JavaScript files could be defined as files with extensions like ".js", ".javascript", ".jsx" etc. Previously it was only possible to provide a single extension per file type like only ".js" for JavaScript files.

--- src/filters/index.js
+++ src/filters/index.js
@@ -9,7 +9,7 @@ module.exports = {
 
     const ext = path.parse(metadata.filename).ext;
     const issues = [];
-    let set = filtersList.filter(f => f.ext === ext);
+    let set = filtersList.filter(f => f.ext.indexOf(ext) > -1);
 
     if (set.length > 1) {
       throw new Error(`More than one object for the same extension "${ext}" specified in a config/filters.json.`);
