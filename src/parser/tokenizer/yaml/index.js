f0adcafeb54babd04540326a788df9d3746002fe
Eliott Thomas <eliott.h.thomas@gmail.com>
2020-03-09 15:53:51 +0000

    add limit for recursive funciton

--- src/parser/tokenizer/yaml/index.js
+++ src/parser/tokenizer/yaml/index.js
@@ -1,16 +1,22 @@
 const yaml = require('js-yaml');
 const { flatten, uniq } = require('lodash');
 
-const toArray = (obj, arr) => {
-  if (obj) {
+// Set recursion limit
+const maxDepth = 50;
+
+const toArray = (obj, arr, depth = 0) => {
+  if (obj && depth < maxDepth) {
+    depth++;
     Object.keys(obj).forEach((key) => {
       const value = obj[key];
       if (value !== null && typeof value === 'object') {
-        toArray(value, arr);
+        toArray(value, arr, depth);
       } else if (value) {
         arr.push(value.toString());
       }
     });
+  } else if (depth >= maxDepth) {
+    throw new Error(`Maximum object depth (${maxDepth}) exceeded.`);
   }
   return arr;
 };
